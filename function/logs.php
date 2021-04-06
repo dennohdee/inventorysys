5f905bda48337ff72596e40453fffa810308c8ae
Kiprotich Dennis <36850270+dennohdee@users.noreply.github.com>
2021-04-06 23:22:23 +0300

    Update logs.php

--- function/logs.php
+++ function/logs.php
@@ -1,7 +1,7 @@
 <?php
 function logger($username){
     $ip = $_SERVER['ip_address'];
-    $log = "INSERT INTO access_log ('username','ip_address') VALUES ($username,$ip)";
+    $log = $connect->query("INSERT INTO access_log ('username','ip_address') VALUES ($username,$ip)");
 }
 ?>
 
